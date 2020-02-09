b65b832a7f3321eca2c6736339692d3c211b2f77
Jay\^.^/ <stsend90@yahoo.com>
2020-02-08 23:17:37 -0500

    re doing schema

--- models/Note.js
+++ models/Note.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
-const noteSchema = new Schema({
+const NoteSchema = new Schema({
   title: String,
   body: String
 });
 
-const Note = mongoose.model("Note", noteSchema);
+const Note = mongoose.model("Note", NoteSchema);
 
 module.exports = Note;
